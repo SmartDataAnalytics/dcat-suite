54ad985d1700e4f174f4e0169b72f3a49609dc06
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2019-08-22 15:15:45 +0200

    fixed broken method reference

--- dcat-suite-binding-ckan/src/main/java/org/aksw/dcat_suite/binding/ckan/jena/plugin/JenaPluginDcatSuiteCkanBinding.java
+++ dcat-suite-binding-ckan/src/main/java/org/aksw/dcat_suite/binding/ckan/jena/plugin/JenaPluginDcatSuiteCkanBinding.java
@@ -8,7 +8,7 @@ import org.apache.jena.sys.JenaSubsystemLifecycle;
 
 public class JenaPluginDcatSuiteCkanBinding
 	implements JenaSubsystemLifecycle
-{
+{	
 	@Override
 	public void start() {
 		init();
