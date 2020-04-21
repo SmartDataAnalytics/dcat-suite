7e32c8249a33f2e8176a0670719403af183be58c
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2020-04-21 17:00:23 +0200

    Added installer

---
+++ dcat-suite-cli/src/main/java/dcat.java
@@ -0,0 +1,7 @@
+import org.aksw.dcat_suite.cli.main.MainCliDcatSuite;
+
+public class dcat {
+    public static void main(String[] args) throws Exception {
+        MainCliDcatSuite.main(args);
+    }
+}
